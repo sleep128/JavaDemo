fad88b271d63eca5955ece5e1b71ce8480752926
sleep128 <1225831948@qq.com>
2018-02-27 23:55:34 +0800

    enum

---
+++ src/main/java/com/sleep/tips/enumDemo/TestOrgEnum.java
@@ -0,0 +1,11 @@
+package com.sleep.tips.enumDemo;
+
+public class TestOrgEnum {
+    public static void main(String[] args) {
+        ErrorCode businessError01 = ErrorCode.BusinessError.BUSINESS_ERROR_01;
+        ErrorCode businessError02 = ErrorCode.BusinessError.BUSINESS_ERROR_02;
+        ErrorCode commonError01 = ErrorCode.CommonError.COMMON_ERROR_01;
+        ErrorCode commonError02 = ErrorCode.CommonError.COMMON_ERROR_02;
+
+    }
+}
